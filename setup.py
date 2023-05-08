a6c334a6c36354730667e788ae517119a5eef87f
kavinari <132856478+kavinari@users.noreply.github.com>
2023-05-08 16:45:49 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='pyRukassa',
+      version='1.0',
+      url='https://github.com/kavinari/pyRukassa',
+      license='MIT',
+      description='rukassa api python wrapper',
+      packages=['pyKeksik'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='kavinari',
+      install_requires=['requests'],
+      author_email='kavinari@gmail.com',
+      zip_safe=False)
