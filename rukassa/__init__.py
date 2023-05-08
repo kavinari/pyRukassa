ee66a1ccef3b8102b17555877e36f34a7fa3fdac
kavinari <132856478+kavinari@users.noreply.github.com>
2023-05-08 21:21:44 +0300

    Update __init__.py

--- rukassa/__init__.py
+++ rukassa/__init__.py
@@ -1,3 +1,4 @@
-from .api import RukassaAPI, PaymentData, PaymentInfo, WithdrawInfo, BalanceInfo, WithdrawResult,  APIError, ValueNotReceived, StoreOrPaymentNotFound, MerchantNotVerified, OtherError
+from .api import RukassaAPI, PaymentData, PaymentInfo, WithdrawInfo, BalanceInfo, WithdrawResult
+from .typing import APIError, ValueNotReceived, StoreOrPaymentNotFound, MerchantNotVerified, OtherError
 
 __all__ = ['RukassaAPI', 'PaymentData', 'PaymentInfo', 'WithdrawInfo', 'BalanceInfo', 'WithdrawResult', 'APIError', 'ValueNotReceived', 'StoreOrPaymentNotFound', 'MerchantNotVerified', 'OtherError']
